d890bdf9946d94bec226929710e7df27e64840cc
LukacsBotond <57891898+LukacsBotond@users.noreply.github.com>
2022-05-19 17:28:15 +0300

    comment out baseSwith and it's controller

--- controll/include/ASwitchController.h
+++ controll/include/ASwitchController.h
@@ -7,7 +7,7 @@
 #include "./IDAC.h"
 
 //works for both Analog and resistor swich mode
-class BASESWITCHCONTROLLER : public ISWITCHCONTROLLER
+class ASWITCHCONTROLLER : public ISWITCHCONTROLLER
 {
 private:
     IASWITCH *aswitch1;
@@ -16,16 +16,16 @@ private:
     IDAC* idac;
 
 public:
-    BASESWITCHCONTROLLER(IASWITCH *sw1, IASWITCH *sw2, IASWITCH *sw3);
-    BASESWITCHCONTROLLER(IASWITCH *sw1, IASWITCH *sw2, IASWITCH *sw3, IDAC *idac);
-    BASESWITCHCONTROLLER(BASESWITCHCONTROLLER &copy);
-    ~BASESWITCHCONTROLLER();
+    ASWITCHCONTROLLER(IASWITCH *sw1, IASWITCH *sw2, IASWITCH *sw3);
+    ASWITCHCONTROLLER(IASWITCH *sw1, IASWITCH *sw2, IASWITCH *sw3, IDAC *idac);
+    ASWITCHCONTROLLER(ASWITCHCONTROLLER &copy);
+    ~ASWITCHCONTROLLER();
 
-    //! TODO implement this in CALCULATE class
-    //void SameOut3ChannelRepeat(uint8_t sw1P, uint8_t sw2P, uint8_t sw3P) override;
+    void setSwithcSetting(const uint32_t mask, const uint32_t value) override;
 
     void setSwithcSetting(const uint8_t swNum, const uint8_t mode) override;
     uint8_t getSwithcSetting(const uint8_t swNum) const override;
+    
     uint getResistorSetting(const uint8_t swNum,const uint8_t resistorNr) const override;
     double getswTotResistorSetting(uint8_t swNum) const  override;
     double getTotResistorFromMode(uint8_t mode) const override;
